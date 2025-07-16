e1918013121d387e836431835bca03c62416cc55
trbutler4 <trbiv@proton.me>
2025-07-16 17:09:21 -0500

    remove pointless tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout() {
     <QueryClientProvider client={queryClient}>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="index" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
         </Stack>
         <StatusBar style="auto" />
